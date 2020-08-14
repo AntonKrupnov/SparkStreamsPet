0e710d9280526ff9d03dc58eded9c4893f032195
Anton <anton.p.krupnov@gmail.com>
2020-08-14 16:27:10 +0300

    Implement 1.Year->TotalCost 2.Brand:Year->Count:TotalCost

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import com.google.common.collect.ImmutableList;
 import org.apache.commons.lang3.SerializationUtils;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.ByteArrayDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.spark.SparkConf;
@@ -13,13 +13,15 @@ import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.kafka010.ConsumerStrategies;
 import org.apache.spark.streaming.kafka010.KafkaUtils;
 import org.apache.spark.streaming.kafka010.LocationStrategies;
+import scala.Tuple2;
 
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         KafkaProducer.startProduceMessagesFromAvroFileToKafka();
 
         SparkConf sparkConf = new SparkConf().setAppName("sparkStreamingPet").setMaster("local[2]");
@@ -37,14 +39,46 @@ public class Main {
                         LocationStrategies.PreferConsistent(),
                         ConsumerStrategies.Subscribe(ImmutableList.of(KafkaProducer.MAIN_TOPIC), parameters));
 
-        System.out.println("Printing...");
 //        javaInputDStream
-//                .foreachRDD((consumerRecordJavaRDD, time) ->
-//                        consumerRecordJavaRDD.foreach(System.out::println));
+//                .map(record -> SerializationUtils.deserialize(record.value()))
+//                .print();
 
-        javaInputDStream.map(record -> SerializationUtils.deserialize(record.value())).print();
+        javaInputDStream
+                .map(record -> (CarAvro) SerializationUtils.deserialize(record.value()))
+                .map(carAvro -> carAvro.getYear() + " -> " + carAvro.getPriceUsd())
+                .print();
+
+        javaInputDStream
+                .map(record -> (CarAvro) SerializationUtils.deserialize(record.value()))
+                .mapToPair(carAvro ->
+                        Tuple2.apply(
+                                carAvro.getBrand() + ":" + carAvro.getYear(),
+                                new CountTotalCost(1, carAvro.getPriceUsd())))
+                .reduceByKey((obj1, obj2) -> new CountTotalCost(
+                        obj1.getCount() + obj2.getCount(),
+                        obj1.getTotalCost() + obj2.getTotalCost()))
+                .map(pair -> pair._1 + " -> " + pair._2.getCount() + ":" + pair._2.getTotalCost())
+                .print();
 
         javaStreamingContext.start();
+    }
+
+    private static class CountTotalCost implements Serializable {
+        int count;
+        double totalCost;
+
+        public CountTotalCost(int count, double totalCost) {
+            this.count = count;
+            this.totalCost = totalCost;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        public double getTotalCost() {
+            return totalCost;
+        }
     }
 
 }
